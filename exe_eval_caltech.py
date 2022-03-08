99d737c9b35a28f5ab3c8973fed19c8221044ede
grueningp <pg@prcmail.de>
2022-03-08 15:57:17 +0100

    add evaluation example

---
+++ exe_eval_caltech.py
@@ -0,0 +1,60 @@
+from ast import arg
+import subprocess
+import os
+import argparse
+
+
+def get_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--index', type=int, default=0)
+    return parser.parse_args()
+
+
+GPUS = [2, 3, 4, 5, 6, 7]
+GPUS = [0]
+
+BITSTRINGS = [
+    '101 111 011100000 000',
+    '000 110 001100000 000',
+    '101 110 111000000 000',
+    '000 110 010100000 000',
+    '100 110 011000010 000',
+    '100 111 111100000 000',
+]
+
+
+def run():
+    options = get_options()
+    idx = options.index
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(GPUS[idx])
+    os.environ["NCCL_IB_DISABLE"] = '1'
+    os.environ["NCCL_SOCKET_IFNAME"] = 'lo'
+
+    call_str = [
+        'python',
+        'main.py',
+        '--model', 'convnext_tiny',
+        '--batch_size', str(32),
+        '--lr', str(4e-3),
+        '--update_freq', str(4),
+        '--model_ema', 'true',
+        '--model_ema_eval', 'true',
+        '--data_path', 'data',
+        #'--output_dir', f'exp_data/caltech_{BITSTRINGS[idx]}',
+        '--output_dir', f'exp_data/caltech',
+        '--log_dir', 'exp_data/test/tb_log',
+        '--data_set', 'CALTECH',
+        '--num_workers', str(0),
+        #'--bitstring', BITSTRINGS[idx],
+        '--drop_path', str(0.1),
+        # EVAL ONLY MODE!
+        '--eval', 'true',
+        '--auto_resume', 'true',
+    ]
+    print(call_str)
+    subprocess.call(call_str)
+
+
+if __name__ == '__main__':
+    run()
